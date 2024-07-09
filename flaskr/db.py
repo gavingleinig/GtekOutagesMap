88fc0152ee66879a052ccc1775a9cedabd8df958
CER <gavingleinig@gmail.com>
2024-07-09 09:03:48 -0500

    WIP database, initial fill

--- flaskr/db.py
+++ flaskr/db.py
@@ -2,6 +2,7 @@ import sqlite3
 
 import click
 from flask import current_app, g
+import csv
 
 
 def get_db():
@@ -26,6 +27,24 @@ def init_db():
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
+    
+    #INSERT INTO towers (name, latitude, longitude, radius, status) VALUES ("Patrick Farms GE",27.6701583531,-97.9140368697,4,"active");
+    #INSERT INTO towers (name, latitude, longitude, radius, status) VALUES ("Alice Fire Department",27.7533776428,-98.0707372187,10,"active");
+    with open('towers.csv', 'r') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        
+        for row in csvreader:
+            name = row['name']
+            longitude = float(row['x coordinate'])
+            latitude = float(row['y coordinate'])
+            radius = int(row['description'])
+            status = "active"
+            
+            # Insert data into the SQLite table
+            cursor.execute('''
+            INSERT INTO towers (name, latitude, longitude, radius, status)
+            VALUES (?, ?, ?, ?, ?)
+            ''', (name, latitude, longitude, radius, status))
 
 
 @click.command('init-db')
